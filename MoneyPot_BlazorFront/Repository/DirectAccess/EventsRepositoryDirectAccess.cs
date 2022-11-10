ad4c7c34dc83b8f36c95eb92b9a5419f81f72e81
Apolixit <friotromain@gmail.com>
2022-11-10 22:35:33 +0100

    Add simple events listener component. WIP to listen all events with enum details

--- MoneyPot_BlazorFront/Repository/DirectAccess/EventsRepositoryDirectAccess.cs
+++ MoneyPot_BlazorFront/Repository/DirectAccess/EventsRepositoryDirectAccess.cs
@@ -1,7 +1,9 @@
 ﻿using Ajuna.NetApi.Model.Rpc;
-using Ajuna.ServiceLayer.Model;
+using Ajuna.NetApi.Model.Types.Base;
 using MoneyPot_BlazorFront.Helpers;
 using MoneyPot_BlazorFront.Service;
+using MoneyPot_NetApiExt.Generated.Model.frame_system;
+using MoneyPot_NetApiExt.Generated.Model.node_template_runtime;
 using MoneyPot_NetApiExt.Generated.Storage;
 using MoneyPot_Shared;
 
@@ -10,26 +12,72 @@ namespace MoneyPot_BlazorFront.Repository.DirectAccess
     public class EventsRepositoryDirectAccess : IEventsRepository
     {
         private readonly ISubstrateService _substrateService;
+        private readonly IBlockRepository _blockRepository;
 
-        public EventsRepositoryDirectAccess(ISubstrateService substrateService)
+        private readonly IList<EventDto> events = new List<EventDto>();
+
+        public EventsRepositoryDirectAccess(ISubstrateService substrateService, IBlockRepository blockRepository)
         {
             _substrateService = substrateService;
+            _blockRepository = blockRepository;
         }
 
-        public Task SubscribeNewEventAsync(Action<EventDto> eventCallback)
+        public async Task SubscribeNewEventAsync(Action<EventDto> eventCallback)
         {
 
-            //var storage = _substrateService.Client.StorageKeyDict;
-            //foreach (var s in storage)
-            //{
-            //    Console.WriteLine($"{s.Key} = {s.Value}");
-            //}
+            await _substrateService.Client.SubscribeStorageKeyAsync(SystemStorage.EventsParams(), async (string subscriptionId, StorageChangeSet storageChangeSet) => {
+                var hexString = SubstrateHelper.getChangesetData(storageChangeSet);
 
-            //var res = storage.Where(x => x.Key.Item1 == "MoneyPot").ToList();
+                // No data
+                if (string.IsNullOrEmpty(hexString)) return;
 
+                var eventsReceived = new BaseVec<EventRecord>();
+                eventsReceived.Create(hexString);
 
+                foreach (EventRecord eventReceived in eventsReceived.Value)
+                {
+                    var eventCore = eventReceived.Event;
+                    var secondaryEvent = string.Empty;
 
-            return Task.CompletedTask;
+                    switch (eventCore.Value)
+                    {
+                        case Event.Balances:
+                            var palletSubEvent = (MoneyPot_NetApiExt.Generated.Model.pallet_balances.pallet.EnumEvent)eventCore.Value2;
+                            secondaryEvent = palletSubEvent.Value.ToString();
+                            break;
+                        case Event.Grandpa:
+                            secondaryEvent = ((MoneyPot_NetApiExt.Generated.Model.pallet_grandpa.pallet.EnumEvent)eventCore.Value2).Value.ToString();
+                            break;
+                        case Event.MoneyPot:
+                            secondaryEvent = ((MoneyPot_NetApiExt.Generated.Model.pallet_money_pot.pallet.EnumEvent)eventCore.Value2).Value.ToString();
+                            break;
+                        case Event.Scheduler:
+                            secondaryEvent = ((MoneyPot_NetApiExt.Generated.Model.pallet_scheduler.pallet.EnumEvent)eventCore.Value2).Value.ToString();
+                            break;
+                        case Event.Sudo:
+                            secondaryEvent = ((MoneyPot_NetApiExt.Generated.Model.pallet_sudo.pallet.EnumEvent)eventCore.Value2).Value.ToString();
+                            break;
+                        case Event.System:
+                            secondaryEvent = ((MoneyPot_NetApiExt.Generated.Model.frame_system.pallet.EnumEvent)eventCore.Value2).Value.ToString();
+                            break;
+                        case Event.TransactionPayment:
+                            secondaryEvent = ((MoneyPot_NetApiExt.Generated.Model.pallet_transaction_payment.pallet.EnumEvent)eventCore.Value2).Value.ToString();
+                            break;
+                    }
+
+                    var newEvent = new EventDto()
+                    {
+                        PalletName = eventCore.Value.ToString(),
+                        EventName = secondaryEvent,
+                        Block = await _blockRepository.GetLastBlockAsync(),
+                        Description = string.Empty
+                    };
+                    events.Add(newEvent);
+
+                    eventCallback(newEvent);
+                }
+
+            }, CancellationToken.None);
         }
     }
 }
--- Shared_MoneyPot/EventDto.cs
+++ Shared_MoneyPot/EventDto.cs
@@ -10,6 +10,7 @@ namespace MoneyPot_Shared
     public class EventDto
     {
         public BlockDto Block { get; set; } = new BlockDto();
+        public string PalletName { get; set; } = string.Empty;
         public string EventName { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
     }
